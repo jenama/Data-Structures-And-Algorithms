c403520d1ebf205fe0a72366895fe82b6ede21b7
jenama <johanneenama@gmail.com>
2020-08-04 23:13:55 -0400

    stacks

---
+++ Stacks/minStack.js
@@ -0,0 +1,70 @@
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+ 
+
+// Example 1:
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+
+const MinStack = () => {
+    this.arr = []
+    
+};
+
+/** 
+//  * @param {number} x
+//  * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+  this.arr.push(x)
+};
+
+/**
+//  * @return {void}
+ */
+MinStack.prototype.pop = function() {
+     this.arr.pop()
+};
+
+/**
+//  * @return {number}
+ */
+MinStack.prototype.top = function() {
+   return this.arr[this.arr.length - 1]
+};
+
+/**
+//  * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    let newArr = this.arr.slice()
+   return Math.min(...this.arr)
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
