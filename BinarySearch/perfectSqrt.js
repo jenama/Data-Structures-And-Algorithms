46c38b8750f91a2f364b9c8e5b3eb9f922217aa1
jenama <johanneenama@gmail.com>
2020-09-03 20:35:58 -0400

    new binary search solution for isPerfectSquare

--- BinarySearch/perfectSqrt.js
+++ BinarySearch/perfectSqrt.js
@@ -7,13 +7,37 @@
 // Input: num = 14
 // Output: false
 
-let num = 101
+// let num = 101
+// const isPerfectSquare = (num) => {
+//    let sqrt = Math.sqrt(num)
+//    if (sqrt % 1 === 0) {
+//        return true
+//    } else {
+//         return false
+//    }
+// };
+// console.log(isPerfectSquare(num))
+
+// Binary Search version
 const isPerfectSquare = (num) => {
-   let sqrt = Math.sqrt(num)
-   if (sqrt % 1 === 0) {
-       return true
-   } else {
-        return false
-   }
+    /* check if the num is less than  to 1 b/c 0*0 = 0 and since
+    num is positive negative numbers will also be false.
+    */
+    if (num < 1) return false
+    let first = 1
+    let last = num
+   while (first <= last) {
+       let mid = Math.floor((first + last)/2)
+       console.log(mid)
+       if (mid * mid === num) return true       
+       
+       if (mid * mid > num) {
+            last = mid - 1
+            
+        } else if (mid * mid < num) {
+           first = mid + 1
+        }
+    }
+    return false
 };
-console.log(isPerfectSquare(num))
+console.log(isPerfectSquare(14))
