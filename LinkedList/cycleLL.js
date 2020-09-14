acd38738dcb466bf42d8a788eceedc6b412b70c1
jenama <johanneenama@gmail.com>
2020-09-14 13:57:24 -0400

    from queues to stacks

--- LinkedList/cycleLL.js
+++ LinkedList/cycleLL.js
@@ -22,16 +22,32 @@ Example 2:
 Input: head = [1,2], pos = 0
 Output: true
 */
-const hasCycle (head) => {
+
+
+const listHead1 = new ListNode(3)
+const n1 = new ListNode(2) 
+const n2 = new ListNode(0) 
+const n3 = new ListNode(-4)
+
+listHead1.next = n1
+n1.next = n2
+n2.next = n3
+n3.next = n1
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+ 
+const hasCycle = (head) => {
     if (!head ||!head.next) return false
     let slow = head
     let fast = head.next
-    while (fast !== null && fast.next !== null) {
+    while (fast && fast.next) {
         if (slow === fast)  return true
         slow = slow.next
         fast = fast.next.next
-            
     }
 return false
 };
-hasCycle([3,2,0,-4])
+console.log(hasCycle(listHead1))
