10ed0dd5c30e64a16f39c99170dbc8722626788d
jenama <johanneenama@gmail.com>
2020-09-16 17:58:05 -0400

    number of recent calls

---
+++ Queues/recentCall.js
@@ -0,0 +1,47 @@
+/* 
+
+Write a class RecentCounter to count recent requests.
+
+It has only one method: ping(int t), where t represents some time in milliseconds.
+
+Return the number of pings that have been made from 3000 milliseconds ago until now.
+
+Any ping with time in [t - 3000, t] will count, including the current ping.
+
+It is guaranteed that every call to ping uses a strictly larger value of t than before.
+
+ 
+
+Example 1:
+
+Input: inputs = ["RecentCounter","ping","ping","ping","ping"], inputs = [[],[1],[100],[3001],[3002]]
+Output: [null,1,2,3,3]
+*/
+
+
+
+class RecentCounter {
+    constructor() {
+        this.request = []
+    }
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+      this.request.push(t)
+ while(this.request[0] < t - 3000){
+        this.request.shift()
+    }
+    return this.request.length 
+};
+
+RecentCounter(t)
+
+
+// Your RecentCounter object will be instantiated and called as such:
+const obj = new RecentCounter()
+const param_1 = obj.ping(t)
+ 
