e9da58401d748035b1e8138a673c4b449ec4de90
Johanne enama <48734085+jenama@users.noreply.github.com>
2023-02-15 20:23:51 -0500

    Update twoSum.js

--- HashTables/twoSum.js
+++ HashTables/twoSum.js
@@ -10,21 +10,17 @@
 let nums = [2, 7, 11, 15]
 let target = 26
 const twoSum = (nums, target) => {
-	let emptyArr = []
-	let obj = {}
-	for (let i = 0; i < nums.length; i++) {
-		let diff = target - nums[i]
-		console.log('diff:', diff)
-		if(obj[diff] >= 0) {
-			
-			console.log('num', i)
-			return [obj[diff], i]
-		} else {
-			obj[nums[i]] = i
-		}
-		console.log(obj)
-	}
-		return emptyArr
+    let emptyArr = []
+    let obj = {}
+    for (let i = 0; i < nums.length; i++) {
+	let diff = target - nums[i]
+	if(obj[diff] >= 0) {
+	    return [obj[diff], i]
+	 } else {
+	    obj[nums[i]] = i
+	 }
+     }
+     return emptyArr
 }
 console.log(twoSum(nums, target))
     
